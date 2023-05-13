7f4e48d1fd1c7b29983ba32798a914d716905e45
Syed Ghani <112149481+sghani001@users.noreply.github.com>
2023-05-13 20:10:41 +0500

    Create 9._Palindrome_Number.py

---
+++ 9._Palindrome_Number.py
@@ -0,0 +1,16 @@
+# Given an integer x, return true if x is a 
+# palindrome
+# , and false otherwise.
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x=str(x)
+        y=x[::-1]
+        if x==y:
+            return True
+        else:
+            return False
